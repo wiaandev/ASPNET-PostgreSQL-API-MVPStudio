ea69c0f4f18168168ebc733fffab83c68b688f93
WiaanDuvenhage-200307 <wiaanduvenhage.dev@gmail.com>
2023-08-15 23:28:08 +0200

    Added DB Context File

---
+++ mvp-studio-api/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvp_studio_api;
+
+namespace testApi
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // TODO: Set Tables
+
+        // public DbSet<UserInfo> Items { get; set; }
+    }
+}
--- mvp-studio-api/Program.cs
+++ mvp-studio-api/Program.cs
@@ -1,3 +1,5 @@
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//TODO: Setup our db connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    IConfiguration config = builder.Configuration;
+    options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
