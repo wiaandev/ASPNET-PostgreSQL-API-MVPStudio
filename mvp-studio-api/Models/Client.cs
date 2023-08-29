20065bec502ef0cfa97cb771ebc71f20bef90f5e
Liam Wedge <21100218@virtualwindow.co.za>
2023-08-29 22:38:21 +0200

    admin auth
    clinet

--- mvp-studio-api/Models/Client.cs
+++ mvp-studio-api/Models/Client.cs
@@ -12,15 +12,15 @@ namespace mvp_studio_api.Models
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
-        public string ImgUrl { get; set; }
+        public string ImgUrl { get; set; } = string.Empty;
 
         [Required]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
         [ForeignKey(nameof(ClientType))]
         public int ClientTypeId { get; set; }
-        public ClientType ClientType { get; set; }
+        public ClientType ClientType { get; set; } 
     }
 }
